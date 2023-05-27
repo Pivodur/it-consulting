7ee5917a2e2c13976b2009751dbdb350d7db1a91
Islam Shekerbekov <megazyamba@gmail.com>
2023-05-27 20:18:35 +0600

    add team-page

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -237,3 +237,21 @@ function accordion() {
 }
 accordion();
 
+const showMore = document.querySelector(".team__button");
+const itemsLength = document.querySelectorAll(".team__item").length;
+
+let itemsCount = 4;
+
+showMore.addEventListener("click", () => {
+  itemsCount +=4;
+  const array = Array.from(document.querySelectorAll(".team__item"));
+  const itemsToShow = array.slice(0, itemsCount);
+
+  itemsToShow.forEach( el => {
+    el.classList.add("is-visible");
+  })
+
+  if (itemsCount === itemsLength) {
+    showMore.style.display = "none";
+  }
+})
